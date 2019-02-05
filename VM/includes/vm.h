c0dc9ccff70db012da0e45f51723236c99610557
Drew FINNIS <dfinnis@e1r4p1.42.fr>
2019-02-05 09:41:43 +0100

    updated .h

--- VM/includes/vm.h
+++ VM/includes/vm.h
@@ -4,4 +4,19 @@
 # include "../../op.h"
 # include "../libft/ft_printf/ft_printf.h"
 
+s_struct	s_champ
+{
+	char			*name;
+	unsigned char	reg[16];
+	t_process		*process;
+	int				alive;
+}			t_champ;
+
+s_struct	s_process;
+{
+	unsigned char	*current;
+	int				duration;
+	
+}
+
 #endif
