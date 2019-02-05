67675e6e9ee335603282f43c00cc01ea0f3b235d
Sarunas VASKELIS <svaskeli@student.42.fr>
2019-02-05 19:02:12 +0100

    op_live

--- VM/includes/vm.h
+++ VM/includes/vm.h
@@ -4,11 +4,32 @@
 # include "../../op.h"
 # include "../libft/ft_printf/ft_printf.h"
 
+typedef struct			s_op
+{
+	char				*name;
+	int					argc;
+	int					*argv;
+	int					id;
+	int					cycles;
+	char				*long_name;
+	int					OCP;
+	int					carry;
+}						t_op;
+
+typedef struct			s_process
+{
+	unsigned char		*current;
+	int					duration;
+	unsigned char		reg[16];
+	int					carry;
+	int					alive;
+}						t_process;
+
 typedef struct			s_champ
 {
 	int					nbr;
-	char		    	*name;
-	char				*comment;
+	char				name[PROG_NAME_LENGTH];
+	char				comment[COMMENT_LENGTH];
 	struct s_process	*process;
 	int			    	alive;
 }						t_champ;
@@ -18,13 +39,5 @@ typedef struct			s_game
 	int					dump;
 	struct s_champ		champ[4];
 }						t_game;
-
-typedef struct			s_process
-{
-	unsigned char		*current;
-	int					duration;
-	unsigned char		reg[16];
-	int					carry;
-}						t_process;
 
 #endif
