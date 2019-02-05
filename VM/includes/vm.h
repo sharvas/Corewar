0b868fda37a642afcca2f910343ee0314e5e0678
Sarunas VASKELIS <svaskeli@student.42.fr>
2019-02-05 11:07:37 +0100

    structures

--- VM/includes/vm.h
+++ VM/includes/vm.h
@@ -4,19 +4,20 @@
 # include "../../op.h"
 # include "../libft/ft_printf/ft_printf.h"
 
-s_struct	s_champ
+typedef struct			s_champ
 {
-	char			*name;
-	unsigned char	reg[16];
-	t_process		*process;
-	int				alive;
-}			t_champ;
+	char		    	*name;
+	char				*comment;
+	struct s_process	*process;
+	int			    	alive;
+}						t_champ;
 
-s_struct	s_process;
+typedef struct			s_process
 {
-	unsigned char	*current;
-	int				duration;
-	
-}
+	unsigned char		*current;
+	int					duration;
+	unsigned char		reg[16];
+	int					carry;
+}						t_process;
 
 #endif
